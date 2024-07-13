26660cdde8ff889142ab066174cf40241de652a2
manas1803 <manasmonupant@gmail.com>
2024-07-13 18:10:17 +0530

    Setup for tailwind

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      My Portfolio
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
 }
 
