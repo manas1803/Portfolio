bdc52e9eea023740eef7bd309543fb4ec77257d5
manas1803 <manasmonupant@gmail.com>
2024-07-13 22:28:29 +0530

    Initial folder structure portfolio

---
+++ portfolio/src/pages/App/App.js
@@ -0,0 +1,11 @@
+import PortfolioHeader from "../PortfolioHeader/PortfolioHeader";
+
+function App() {
+  return (
+    <>
+    <PortfolioHeader />
+    </>
+  );
+}
+
+export default App;
---
+++ portfolio/src/pages/PortfolioHeader/PortfolioHeader.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const PortfolioHeader = () => {
+  return (
+    <>
+      <header>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Projects</li>
+                <li>Skills</li>
+                <li>Blog</li>
+            </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default PortfolioHeader;
