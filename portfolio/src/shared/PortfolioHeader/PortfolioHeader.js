fd00efeb16b60a7cd87d6d7fff98d92c6f9f0d68
manas1803 <manasmonupant@gmail.com>
2024-07-15 22:15:01 +0530

    Updated CSS for the navBar

--- portfolio/src/shared/PortfolioHeader/PortfolioHeader.js
+++ portfolio/src/shared/PortfolioHeader/PortfolioHeader.js
@@ -3,33 +3,30 @@ import { NavLink } from "react-router-dom";
 
 const PortfolioHeader = () => {
   return (
-      <header className="flex flex-row border-2 border-rose-700 px-10 py-64 bg-teal-900 text-teal-50">
-        <nav>
-          <ul>
-          <li>
-              <img src="" alt="Profile Photo" />
-            </li>
-            <li>
-              <NavLink to="/about">About</NavLink>
-            </li>
-            <li>
-              <NavLink to="/projects">Projects</NavLink>
-            </li>
-            <li>
-              <NavLink to="/education">Education</NavLink>
-            </li>
-            <li>
-              <NavLink to="/skills">Skills</NavLink>
-            </li>
-            <li>
-              <NavLink to="/interests">Interests</NavLink>
-            </li>
-            <li>
-              <NavLink to="/social">Social</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
+    <header className="flex flex-row px-20 py-40 bg-teal-900 text-teal-50">
+      <nav>
+        <ul>
+          <li className="my-4 text-center hover:text-teal-300 hover:scale-110 transition ease-in-out delay-150 ">
+            <NavLink to="/about" className="aria-[current=page]:text-teal-300">ABOUT</NavLink>
+          </li>
+          <li className="my-4 text-center hover:text-teal-300 hover:scale-110 transition ease-in-out delay-150">
+            <NavLink to="/projects" className="aria-[current=page]:text-teal-300">PROJECTS</NavLink>
+          </li>
+          <li className="my-4 text-center hover:text-teal-300 hover:scale-110 transition ease-in-out delay-150 ">
+            <NavLink to="/education" className="aria-[current=page]:text-teal-300">EDUCATION</NavLink>
+          </li>
+          <li className="my-4 text-center hover:text-teal-300 hover:scale-110 transition ease-in-out delay-150 ">
+            <NavLink to="/skills" className="aria-[current=page]:text-teal-300">SKILLS</NavLink>
+          </li>
+          <li className="my-4 text-center hover:text-teal-300 hover:scale-110 transition ease-in-out delay-150 ">
+            <NavLink to="/interests" className="aria-[current=page]:text-teal-300">INTERESTS</NavLink>
+          </li>
+          <li className="my-4 text-center focus:text-teal-300 hover:text-teal-300 hover:scale-110 transition ease-in-out delay-150">
+            <NavLink to="/social" className="aria-[current=page]:text-teal-300">SOCIAL</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
