72f06aad64a274e20875348b76458775bdee3d21
manas1803 <manasmonupant@gmail.com>
2024-07-19 00:20:43 +0530

    Added structure for about

--- portfolio/src/pages/About/About.js
+++ portfolio/src/pages/About/About.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from "react";
 
 export const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <main>
+      <div>
+        <div>Manas Pant</div>
+        <address>
+          Bangalore,Karnataka{" "}
+          <a href="mailto:manaspant18031997@gmail.com">email</a>
+        </address>
+      </div>
+      <article>
+        With 4.5 years in React and Angular, I excel in UI developement along
+        with performance improvement using Webpack, and testing with React
+        Testing Library.I'm a dedicated team player committed to continual
+        learning and delivering high-quality code.
+      </article>
+      <article>
+        <span>Linkedin</span>
+        <span>Github</span>
+        <span>Blogs</span>
+      </article>
+    </main>
+  );
+};
